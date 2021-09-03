f66f0fa7cf763f4ba2e1f79448c158b473bc061f
Adnan Valdes <adravaldes+github@gmail.com>
2021-09-03 15:52:45 -0700

    first commit

---
+++ master/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+app_name = "records"
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("documents/", views.documents, name='documents')
+
+]
