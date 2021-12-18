50f3485d4f8a12726458cfc0b4ba64ca123238ed
Adnan Valdes <adravaldes+github@gmail.com>
2021-12-17 16:21:46 -0800

    Move files into Django framework

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MasterConfig(AppConfig):
+class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'master'
+    name = 'catalog'
