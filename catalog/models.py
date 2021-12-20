4d4bcd6a33486e6a6ab328b7df9ca1544a4531b2
Adnan Valdes <adravaldes+github@gmail.com>
2021-12-20 12:45:49 -0800

    Migrate initial models

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 # Create your models here.
 class Genre(models.Model):
@@ -33,3 +34,50 @@ class Book(models.Model):
 	def get_absolute_url(self):
 		"""Returns the URL to access a detail record for this book"""
 		return reverse('book-detail', args=[str(self.id)])
+
+class BookInstance(models.Model):
+	"""Model representing a specific copy of a book"""
+	# UUIDField is used for the id field to set it as the primary_key for this model. This type of field allocates a globally unique value for each instance (one for every book you can find in the library).
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+	help_text="Unique ID for this particular book")
+	book = models.ForeignKey("Book", on_delete=models.RESTRICT, null=True)
+	imprint = models.CharField(max_length=200)
+	due_back = models.DateField(null=True, blank=True)
+
+	LOAN_STATUS = (
+		("m", "Maintenance"),
+		("o", "On Loan"),
+		("a", "Available"),
+		("r", "Reserved")
+	)
+
+	status = models.CharField(max_length=1,
+							  choices=LOAN_STATUS,
+							  blank=True,
+							  default="m",
+							  help_text="Book availability"
+	)
+
+	class Meta:
+		ordering = ['due_back']
+
+	def __str__(self):
+		return f"{self.id} ({self.book.title})"
+
+
+class Author(models.Model):
+	"""Model representing an author"""
+	fname = models.CharField(max_length=100)
+	sname = models.CharField(max_length=100)
+	date_of_birth = models.DateField(null=True, blank=True)
+	date_of_death = models.DateField("Died", null=True, blank=True)
+
+	class Meta:
+		ordering = ['sname', 'fname']
+
+	def get_absolute_url(self):
+		"""Returns URL to access a particular author instance"""
+		return reverse('author-detail', args=[str(self.id)])
+
+	def __str__(self):
+		return f"{self.sname}, {self.fname}"
