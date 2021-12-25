0c836a4e81f4fa0885552844ffd6eb0ee1eaa032
Adnan Valdes <adravaldes+github@gmail.com>
2021-12-25 14:20:48 -0800

    Add inline editing to Book, format BookInstance admin view

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,10 +10,15 @@ admin.site.register(Genre)
 admin.site.register(Language)
 # admin.site.register(BookInstance)
 
+class BookInline(admin.TabularInline):
+	model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
 	list_display = ('sname', 'fname', 'date_of_birth', 'date_of_death')
 
 	fields = ['fname', 'sname', ('date_of_birth', 'date_of_death')]
+
+	inlines = [BookInline]
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -24,10 +29,12 @@ class BooksInstanceInline(admin.TabularInline):
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('title', 'author', 'display_genre')
 
-	inlines = [BooksInstanceInline]
+	inlines = [BooksInstanceInline] # Adds inline section to add book instances on admin
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+	list_display = ('book', 'status', 'due_back', 'id')
+
 	list_filter = ('status', 'due_back')
 	fieldsets = (
 		(None, { # The first item of the tuple is the name of the section
